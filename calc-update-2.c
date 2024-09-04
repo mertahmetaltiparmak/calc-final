dfc9fc8a37c913d0cd6eaf055bd3672e0655f041
mertahmetaltiparmak <mertahmetaltiparmak@gmail.com>
2024-09-04 23:12:34 +0300

    refactor

--- calc-update-2.c
+++ calc-update-2.c
@@ -6,151 +6,120 @@ int main()
     int i, factorial, option, result, numberOne, numberTwo, average, sum;
     factorial = 1;
 
-    while(option != 0){
+    while(option != 0) {
+        printf("1 - Toplama\n");
+        printf("2 - Cikarma\n");
+        printf("3 - Carpma\n");
+        printf("4 - Bolme\n");
+        printf("5 - Faktoriyal\n");
+        printf("6 - Pozitif sayilarin Aritmatik Ort.\n");
+        printf("0 - Cikis\n\n");
 
-    printf("1 - Toplama\n");
-    printf("2 - Cikarma\n");
-    printf("3 - Carpma\n");
-    printf("4 - Bolme\n");
-    printf("5 - Faktoriyal\n");
-    printf("6 - Pozitif sayilarin Aritmatik Ort.\n");
-    printf("0 - Cikis\n\n");
+        printf("Yapmak istediginiz islemi secin:");
+        scanf("%d", &option);
+        printf("\n");
 
-    printf("Yapmak istediginiz islemi secin:");
-    scanf("%d", &option);
-    printf("\n");
+        switch(option){
+            case 1:
+                printf("birinci sayiyi girin:");
+                scanf("%d", &numberOne);
+                printf("\n");
 
-    switch(option){
+                printf("ikinci sayiyi girin:");
+                scanf("%d", &numberTwo);
+                printf("\n");
 
-    case 1:
+                result = numberOne + numberTwo;
+                printf("Sonuc:%d\n\n", result);
 
-    printf("birinci sayiyi girin:");
-    scanf("%d", &numberOne);
-    printf("\n");
+                break;
+            case 2:
+                printf("birinci sayiyi girin:");
+                scanf("%d", &numberOne);
+                printf("\n");
 
-    printf("ikinci sayiyi girin:");
-    scanf("%d", &numberTwo);
-    printf("\n");
+                printf("ikinci sayiyi girin:");
+                scanf("%d", &numberTwo);
+                printf("\n");
 
-    result = numberOne + numberTwo;
+                result = numberOne - numberTwo;
+                printf("Sonuc:%d\n\n", result);
 
-    printf("Sonuc:%d\n\n", result);
+                break;
+            case 3:
+                printf("birinci sayiyi girin:");
+                scanf("%d", &numberOne);
+                printf("\n");
 
-    break;
+                printf("ikinci sayiyi girin:");
+                scanf("%d", &numberTwo);
+                printf("\n");
 
-    case 2:
+                result = numberOne * numberTwo;
+                printf("Sonuc:%d\n\n", result);
 
-    printf("birinci sayiyi girin:");
-    scanf("%d", &numberOne);
-    printf("\n");
+                break;
+            case 4:
+                printf("birinci sayiyi girin:");
+                scanf("%d", &numberOne);
+                printf("\n");
 
-    printf("ikinci sayiyi girin:");
-    scanf("%d", &numberTwo);
-    printf("\n");
+                printf("ikinci sayiyi girin:");
+                scanf("%d", &numberTwo);
+                printf("\n");
 
-    result = numberOne - numberTwo;
+                result = numberOne / numberTwo;
+                printf("Sonuc:%d\n\n", result);
 
-    printf("Sonuc:%d\n\n", result);
+                break;
+            case 5:
+                printf("sayi girin:");
+                scanf("%d", &numberOne);
+                printf("\n");
 
-    case 3:
+                if(numberOne < 0) {
+                    printf("Gecersiz deger\n\n");
+                    return 0;
+                }
 
-    printf("birinci sayiyi girin:");
-    scanf("%d", &numberOne);
-    printf("\n");
+                for(i = 1; i <= numberOne; i++) {
+                    factorial *= i;
+                }
+                printf("%d! = %d\n\n", numberOne, factorial);
 
-    printf("ikinci sayiyi girin:");
-    scanf("%d", &numberTwo);
-    printf("\n");
+                break;
+            case 6:
+                // hatali calismiyor.
 
-    result = numberOne * numberTwo;
+                printf("Kac sayi gireceksiniz:");
+                scanf("%d", &numberOne);
+                printf("\n");
 
-    printf("Sonuc:%d\n\n", result);
+                if(numberOne <= 0) {
+                    printf("Gecersiz deger\n");
+                    return 0;
+                }
 
-    break;
+                for(i = 1; i <= numberOne; i++) {
+                    printf("Sayi girin:");
+                    scanf("%d", &numberTwo);
+                    printf("\n");
 
-    case 4:
+                    if(numberTwo == 0 || numberTwo < 0) {
+                        printf("Lutfen pozitif bir sayi girin.\n");
+                        return 0;
+                    } else {
+                        sum += numberTwo;
+                    }
 
-    printf("birinci sayiyi girin:");
-    scanf("%d", &numberOne);
-    printf("\n");
+                    average = sum / numberOne;
+                    printf("aritmatik ortalama:%d\n\n", average);
+                }
+                break;
+            case 0:
+                break;
 
-    printf("ikinci sayiyi girin:");
-    scanf("%d", &numberTwo);
-    printf("\n");
-
-    result = numberOne / numberTwo;
-
-    printf("Sonuc:%d\n\n", result);
-
-    break;
-
-    case 5:
-
-    printf("sayi girin:");
-    scanf("%d", &numberOne);
-    printf("\n");
-
-    if(numberOne < 0){
-
-    printf("Gecersiz deger\n\n");
-
-    return 0;
+            return 0;
+        }
     }
-
-    for(i = 1; i <= numberOne; i++)
-
-    factorial *= i;
-
-    printf("%d! = %d\n\n", numberOne, factorial);
-
-    break;
-
-    case 6:
-
-    // hatali calismiyor.
-
-    printf("Kac sayi gireceksiniz:");
-    scanf("%d", &numberOne);
-    printf("\n");
-
-    if(numberOne <= 0){
-
-    printf("Gecersiz deger\n");
-
-    return 0;
-    }
-    for(i = 1; i <= numberOne; i++){
-
-    printf("Sayi girin:");
-    scanf("%d", &numberTwo);
-    printf("\n");
-
-    if(numberTwo == 0 || numberTwo < 0){
-
-    printf("Lutfen pozitif bir sayi girin.\n");
-
-    return 0;
-    }
-
-    else{
-
-    sum += numberTwo;
-
-    }
-    average = sum / numberOne;
-
-    printf("aritmatik ortalama:%d\n\n", average);
-
-    break;
-
-    case 0:
-
-    break;
-    }
-
-    return 0;
-}
-
-    }
-
 }
